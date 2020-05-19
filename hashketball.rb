48e7cecca7744780b7ef572163ebdcf52ed61409
Travis Prol <travisjohnprol@gmail.com>
2020-05-19 12:45:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => [Black, White],
+      :colors => ["Black", "White"],
       :players => [
         {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
         {player_name: "Reggie Evans", number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
@@ -12,14 +12,14 @@ def game_hash
       ]
     },
     :away => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => [
-      #  {player_name: number: shoe: points: rebounds: assists: steals: blocks: slam_dunks:},
-      #  {player_name: number: shoe: points: rebounds: assists: steals: blocks: slam_dunks:},
-      #  {player_name: number: shoe: points: rebounds: assists: steals: blocks: slam_dunks:},
-      #  {player_name: number: shoe: points: rebounds: assists: steals: blocks: slam_dunks:},
-      #  {player_name: number: shoe: points: rebounds: assists: steals: blocks: slam_dunks:}
+        {player_name: "Jeff Adrien", number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
+        {player_name: "Bismack Biyombo", number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 22, blocks: 15, slam_dunks: 10},
+        {player_name: "DeSanga Diop", number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5},
+        {player_name: "Ben Gordon", number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0},
+        {player_name: "Kemba Walker", number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 7, blocks: 5, slam_dunks: 12}
       ]
     }
   }
